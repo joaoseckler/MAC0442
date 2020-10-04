009e4d1c6fd6dd991e347d9788f93c227dece63c
Emanuel Lima <emanuel-lima@outlook.com>
2020-10-04 11:37:38 -0300

    Reorganize the shell

--- Shell/bccsh.c
+++ Shell/bccsh.c
@@ -1,25 +1,18 @@
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <signal.h>
+#include <unistd.h>
 
+#include "aux.h"
 #include "builtin.h"
-
-#define MAX_WORDS 50
-#define MAX_CWD_LENGTH 200
-#define MAX_PROMPT_LENGTH 250
-
-void split(char *, char **);
-void handler();
-void handler_parent();
 
 int main()
 {
-    char* command, * arg, * user, cwd[MAX_CWD_LENGTH], prompt[MAX_PROMPT_LENGTH];
+    char *command, *arg, *user, cwd[MAX_CWD_LENGTH], prompt[MAX_PROMPT_LENGTH];
     char* ptrv[MAX_WORDS];
     int wstatus, err;
     pid_t child;
@@ -77,33 +70,4 @@ int main()
     printf("\n");
 
     return EXIT_SUCCESS;
-}
-
-
-void split(char* string, char ** ptrv)
-{
-    int k = 0, flag = 1;
-    for (int i = 0; string[i] != '\0' && k < MAX_WORDS; i++) {
-        if (string[i] == ' ') {
-            string[i] = '\0';
-            flag = 1;
-        }
-        else if (flag) {
-            ptrv[k] = string + i;
-            flag = 0;
-            k++;
-        }
-    }
-    ptrv[k] = NULL;
-}
-
-void handler() {
-    printf("\n");
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
-}
-
-void handler_parent() {
-  printf("\n");
 }
